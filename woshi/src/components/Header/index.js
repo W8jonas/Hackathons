2d1666a16d264ff8550dfbcdd082c370fc7bc308
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-20 21:33:42 -0300

    terminando header

--- woshi/src/components/Header/index.js
+++ woshi/src/components/Header/index.js
@@ -11,14 +11,14 @@ import Close from '../../assets/icons/close.png'
 import { RectButton } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 
-import { Image, Text, View} from 'react-native'
+import { Image, StyleSheet, View,} from 'react-native'
 
 
 const Header = () => {
     const navigation = useNavigation()
 
   return (
-    <View style={{width: '100%', height: 100, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginTop: 10}}>
+    <View style={style.container}>
         <RectButton onPress={()=>navigation.openDrawer()}>
             <Image source={HeaderImage} resizeMode={"contain"} style={{width: 25, height: 40}} />
         </RectButton>
@@ -31,5 +31,19 @@ const Header = () => {
     </View>
   )
 }
+
+const style = StyleSheet.create({
+	container: {
+		width: '100%', 
+		height: 50, 
+		flexDirection: 'row', 
+		justifyContent: 'space-around', 
+		alignItems: 'center', 
+		marginTop: 40,
+		borderBottomWidth: 1,
+		borderBottomColor: '#000',
+		elevation: 0
+	}
+})
 
 export default Header
