ae7d4b3aa3071081046d5e02b7d257873532a76f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-21 01:47:07 -0300

    adicionando rota  calendario

--- woshi/src/routes.js
+++ woshi/src/routes.js
@@ -5,42 +5,39 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
-import { Button, Text, View } from 'react-native'
 
-
-// function HomeScreen({ navigation }) {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Button
-//           onPress={() => navigation.navigate('Notifications')}
-//           title="Go to notifications"
-//         />
-//       </View>
-//     );
-// }
-
-// function NotificationsScreen({ navigation }) {
-//     return (
-//         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Button onPress={() => navigation.goBack()} title="Go back home" />
-//         </View>
-//     );
-// }
+import Header from './components/Header/index'
 
 import OnlineOffice from './screens/OnlineOffice'
 import Profile from './screens/Profile'
-import Header from './components/Header/index'
+import Calendar from './screens/Calendar'
+
 
 export default function App() {
     return (
 		<NavigationContainer>
-			<Drawer.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+			<Drawer.Navigator 
+				initialRouteName="Home" 
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+
 			<Drawer.Screen 
 				name="Escritório" 
 				component={OnlineOffice}
 				options={{
 					headerShown: true, 
-					header: () => <Header/>
+					header: () => <Header/>,
+				}}
+			/>
+
+			<Drawer.Screen 
+				name="Calendário" 
+				component={Calendar}
+				options={{
+					headerShown: true, 
+					header: () => <Header/>,
 				}}
 			/>
 
