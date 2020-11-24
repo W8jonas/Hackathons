7e02f23c8aee4636d3cf85ca746f8be2d25cc962
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-24 20:59:26 -0300

    corrigindo problema de sombra no header

--- woshi/src/components/Header/index.js
+++ woshi/src/components/Header/index.js
@@ -16,35 +16,42 @@ import { Image, StyleSheet, View,} from 'react-native'
 const Header = () => {
     const navigation = useNavigation()
 
-  return (
-    <View style={style.container}>
-        <RectButton onPress={()=>navigation.openDrawer()}>
-            <Image source={HeaderImage} resizeMode={"contain"} style={{width: 25, height: 40}} />
-        </RectButton>
+  	return (
+		<View style={style.container}>
+			<View style={style.headerContainer}>
+				<RectButton onPress={()=>navigation.openDrawer()}>
+					<Image source={HeaderImage} resizeMode={"contain"} style={{width: 25, height: 40}} />
+				</RectButton>
 
-        <Image source={Woshi} resizeMode={"contain"} style={{width: 100, height: 40}} />
+				<Image source={Woshi} resizeMode={"contain"} style={{width: 100, height: 40}} />
 
-        <RectButton>
-            <Image source={Close} resizeMode={"contain"} style={{width: 25, height: 40}} />
-        </RectButton>
-    </View>
-  )
+				<RectButton>
+					<Image source={Close} resizeMode={"contain"} style={{width: 25, height: 40}} />
+				</RectButton>
+			</View>
+		</View>
+  	)
 }
 
 const style = StyleSheet.create({
 	container: {
 		width: '100%', 
+		height: 100, 
 		backgroundColor: '#FFF',
-    height: 90, 
-    paddingTop: 40,
-    paddingHorizontal: 20,
+	},
+	headerContainer: {
+		width: '100%', 
+		backgroundColor: '#FFF',
+		height: 90, 
+		paddingTop: 40,
+		paddingHorizontal: 20,
 		flexDirection: 'row', 
 		justifyContent: 'space-between', 
 		alignItems: 'center', 
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    elevation: 10,
-    position: 'relative',
+		position: 'relative',
+		borderBottomLeftRadius: 20,
+		borderBottomRightRadius: 20,
+		elevation: 10,
 	}
 })
 
